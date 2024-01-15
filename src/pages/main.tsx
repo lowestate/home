bb38f6cbd320c2f30bc820d2830cfbd2ee4fb096
Yan <bents.yan1@gmail.com>
2024-01-15 21:10:09 +0300

    gpages added upd

--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -1,26 +1,35 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import { Routes, Route } from 'react-router-dom';
-import HomePage from './homePage'
-import ResPage from './resPage'
+import { HashRouter } from 'react-router-dom'
+//import { Routes, Route } from 'react-router-dom';
+//import HomePage from './homePage'
+//import ResPage from './resPage'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Provider } from 'react-redux';
-import AccountPage from './accountPage';
-import AuthPage from './authPage';
-import NavBar from '../components/navigation/NavBar';
-import Breadcrumbs from '../components/breadcrumbs/Breadcrumbs';
+//import AccountPage from './accountPage';
+//import AuthPage from './authPage';
+//import NavBar from '../components/navigation/NavBar';
+//import Breadcrumbs from '../components/breadcrumbs/Breadcrumbs';
 import store from '../store/store';
-import ReportsPage from './reportsPage';
-import ReportDetailedPage from './reportDetailedPage';
-import ManageResources from './manageResourcesPage';
-import NewResPage from './newResourcePage';
-import EditPage from './editPage';
+//import ReportsPage from './reportsPage';
+//import ReportDetailedPage from './reportDetailedPage';
+//import ManageResources from './manageResourcesPage';
+//import NewResPage from './newResourcePage';
+//import EditPage from './editPage';
+import App from './app';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <HashRouter>
+        <App />
+      </HashRouter>
+    </Provider>
+  </React.StrictMode>
+)
+
+/*
+<BrowserRouter>
         <NavBar />
         <Breadcrumbs />
         <Routes>
@@ -39,6 +48,4 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
           
         </Routes>
       </BrowserRouter>
-    </Provider>
-  </React.StrictMode>
-)
+      */
