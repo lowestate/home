82fc3690580c1a59d0b6d633ccbd8ce326eb710a
Yan <bents.yan1@gmail.com>
2024-01-15 17:41:18 +0300

    gpages added upd

--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { Routes, Route } from 'react-router-dom';
 import HomePage from './homePage'
 import ResPage from './resPage'
@@ -20,7 +20,7 @@ import EditPage from './editPage';
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <HashRouter>
         <NavBar />
         <Breadcrumbs />
         <Routes>
@@ -38,7 +38,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
           <Route path="/resources-front/:resource_name" Component={ResPage} />
           
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </Provider>
   </React.StrictMode>
 )
